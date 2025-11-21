e77cc6483f4bef2f2b1499582a7c8f2088ee9a54
nasstyar <anastasiakoz019@gmail.com>
2025-11-21 21:09:04 +0300

    выполнила 2 задание

--- task_2/main.py
+++ task_2/main.py
@@ -1,29 +1,25 @@
-print ("start code") #вывод строки
+#Козлова 1 вариант
+print("start code ...")
 import json
-num = str(input("Введите номер квалификации: "))
-file = "dump.json"
-skills = False
-with open(file, 'r', encoding='utf-8') as file:
-    data = json.load(file)
-    for skill in data:
-        if skill.get("model") == "data.skill":
-            skill_code = skill["fields"].get("code")
-            skill_title = skill["fields"].get("title")
-            skills = True
-            # speciality_code = ""#jfnv ;ajsefv;ajefsv;bajefwb
-            # speciality_title = "" #ajenv;ujefv
-            # speciality_educational = "" #eugvaegbv;ae
-            for profession in data:
-                if profession.get("model") == "data.speciality":
-                    speciality_code = profession["fields"].get("code")
-                    if speciality_code in num:
-                        speciality_title = profession["fields"].get("title")
-                        speciality_educational = proffesion["fields"].get("c_type")
-                        break
-            break
-if not skills:
-    print("=============== Не найдено ===============")
+with open("dump.json","r",encoding="utf-8") as file:
+    data=json.load(file)
+    skills=[]
+    for item in data:
+        if item.get("model")=="data.skill":
+            skills.append({
+            "code":item["fields"]["code"],
+            "title":item["fields"]["title"]
+        })
+user_str=input("Введите номер квалификации: ").strip()
+
+found=[]
+for skill in skills:
+    if user_str == skill["code"]:
+        found.append(skill)
+if found:
+    print("=" * 20 + "Найдено" + "=" * 20)
+    for skill in found:
+        print(f'{skill["code"]} >> Специальность "{skill["title"]}"')
 else:
-    print("=============== Найдено ===============")      
-    print(f"{speciality_code} >> Специальность {speciality_title} , {speciality_educational}")
-    print(f"{skill_code} >> Квалификация {skill_title}")  
+            print("=" * 20 + "Не найдено" + "=" * 20)
+print("... end code")
